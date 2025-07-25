279970359bb323dcb96b33573e026662cdecbf5f
Darren Huang <huan2137@purdue.edu>
2025-07-25 00:13:19 -0400

    components

---
+++ flopr.sv
@@ -0,0 +1,13 @@
+module flopr #(parameter WIDTH = 8) (
+    input logic clk, reset,
+    input logic [WIDTH-1:0] d,
+    output logic [WIDTH-1:0] q
+);
+    always_ff @(posedge clk, posedge reset) begin
+        if (reset) begin
+            q <= 0;
+        end else begin 
+            q <= d;
+        end
+    end
+endmodule
